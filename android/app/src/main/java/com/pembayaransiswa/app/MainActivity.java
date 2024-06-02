bb7550c2ffb4ae3ac0a5cd998a807004e6175ebd
dinul <dinulislami70@gmail.com>
2024-06-02 10:52:56 +0700

    init

---
+++ android/app/src/main/java/com/pembayaransiswa/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pembayaransiswa.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/lib/variables.ts
@@ -0,0 +1,2 @@
+export const API_URL = 'https://fundraiser-backend.vercel.app'
+export const secret = 'kjnedifhu287wo3hfinwjinefuighw223ghfn2qw3if_3029jwd20in'
